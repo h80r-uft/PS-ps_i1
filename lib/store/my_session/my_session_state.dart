5ce08f2e8d7f9751df6dafd05994f0499db21359
h80r <h80rgbc@gmail.com>
2021-05-29 17:53:14 -0300

    feature/redux_setup | app_state & my_session_state
    - Adicionei o estado da sessão vigente:
        - Inclui estado inicial
    - Adicionei o estado do aplicativo:
        - Inclui estado inicial

---
+++ lib/store/my_session/my_session_state.dart
@@ -0,0 +1,36 @@
+import 'package:ps_i1/models/user.dart';
+
+class MySessionState {
+  final User? user;
+  final String? userError;
+
+  final bool loading;
+  final String? loadingError;
+
+  MySessionState({
+    required this.user,
+    this.userError,
+    required this.loading,
+    this.loadingError,
+  });
+
+  factory MySessionState.initial() {
+    return MySessionState(
+      user: null,
+      loading: false,
+    );
+  }
+
+  MySessionState copyWith({
+    User? user,
+    String? userError,
+    bool? loading,
+    String? loadingError,
+  }) {
+    return MySessionState(
+        user: user ?? this.user,
+        userError: userError ?? this.userError,
+        loading: loading ?? this.loading,
+        loadingError: loadingError ?? this.loadingError);
+  }
+}
