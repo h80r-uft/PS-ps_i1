9f2e17bebe1e365f8264cf57173a133d0704974b
h80r <h80rgbc@gmail.com>
2021-05-30 18:31:44 -0300

    support/documentation | documented models
    - Documentei os modelos gerados até então:
       - Modelo de estudantes
       - Modelo de professores
       - Modelo de usuários

--- lib/models/student.dart
+++ lib/models/student.dart
@@ -1,7 +1,14 @@
 import 'package:ps_i1/models/user.dart';
 
+/// Modelo para a criação de estudantes.
+///
+/// Armazena as notas do estudante e seus
+/// atributos de identificação.
 class Student extends User {
+  /// Primeira nota do estudante.
   final double firstGrade;
+
+  /// Segunda nota do estudante.
   final double secondGrade;
 
   Student(User user, {required this.firstGrade, required this.secondGrade})
