d0a0ec4d7fc537244241a10bcb668b9cd080c519
h80r <h80rgbc@gmail.com>
2021-06-02 17:59:52 -0300

    feature/login_page | Refactor
    - Refatorei a página:
       - Criação de um novo widget:
          - Input, reduz a redundância.
       - Uso adequado de `const`
    - Ajustei o tema do input para evitar o background branco duplo, que
      estava anteriormente

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -3,98 +3,115 @@ import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 
 firebase_auth.FirebaseAuth firebaseAuth = firebase_auth.FirebaseAuth.instance;
 
+class Input extends StatelessWidget {
+  final bool isPassword;
+  final String hint;
+  final Widget? suffix;
+
+  const Input({
+    Key? key,
+    required this.hint,
+    this.isPassword = false,
+    this.suffix,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const border = OutlineInputBorder();
+
+    return Container(
+      margin: const EdgeInsets.all(12),
+      width: 400,
+      height: 35,
+      child: TextFormField(
+        obscureText: isPassword,
+        style: const TextStyle(fontSize: 16, color: Colors.black87),
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
+          contentPadding: const EdgeInsets.only(left: 8.0),
+          border: border,
+          focusedBorder: border,
+          hintText: hint,
+          hintStyle: const TextStyle(color: Colors.black38, fontSize: 16),
+          suffixIcon: suffix,
+        ),
+      ),
+    );
+  }
+}
+
 class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Fazer Login'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: const EdgeInsets.all(8.0),
-                color: Colors.white,
-                width: 400,
-                height: 35,
-                child: TextFormField(
-                  style: TextStyle(fontSize: 16, color: Colors.black87),
-                  decoration: InputDecoration(
-                      contentPadding: EdgeInsets.only(left: 8.0),
-                      border: OutlineInputBorder(),
-                      focusedBorder: OutlineInputBorder(),
-                      hintText: 'Email',
-                      hintStyle:
-                          TextStyle(color: Colors.black38, fontSize: 16)),
+      appBar: AppBar(
+        title: const Text('Fazer Login'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Input(hint: 'Email'),
+            Input(
+              hint: 'Senha',
+              isPassword: true,
+              suffix: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.visibility,
+                  size: 18,
+                  color: Colors.black45,
                 ),
               ),
-              Container(
-                margin: const EdgeInsets.all(12),
-                color: Colors.white,
-                width: 400,
-                height: 35,
-                child: TextFormField(
-                  obscureText: true,
-                  style: TextStyle(fontSize: 16, color: Colors.black87),
-                  decoration: InputDecoration(
-                      contentPadding: EdgeInsets.only(left: 8.0),
-                      border: OutlineInputBorder(),
-                      focusedBorder: OutlineInputBorder(),
-                      hintText: 'Senha',
-                      hintStyle: TextStyle(color: Colors.black38, fontSize: 16),
-                      suffixIcon: IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.visibility,
-                              size: 18, color: Colors.black45))),
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 12),
-                width: 400,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    SizedBox(
-                      width: 135,
-                      height: 20,
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          primary: Colors.grey,
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 12, right: 12),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  SizedBox(
+                    width: 135,
+                    height: 20,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
                         ),
-                        child: const Text(
-                          'Esqueci a senha',
-                          style: TextStyle(
-                            color: Colors.black,
-                          ),
+                        primary: Colors.grey,
+                      ),
+                      child: const Text(
+                        'Esqueci a senha',
+                        style: TextStyle(
+                          color: Colors.black,
                         ),
                       ),
                     ),
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Container(
-                width: 400,
-                height: 40,
-                margin: const EdgeInsets.only(top: 20),
-                child: OutlinedButton(
-                  onPressed: () {},
-                  child: const Text(
-                    'Entrar',
                   ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              width: 400,
+              height: 40,
+              margin: const EdgeInsets.only(top: 20, left: 12, right: 12),
+              child: OutlinedButton(
+                onPressed: () {},
+                child: const Text(
+                  'Entrar',
                 ),
-              )
-            ],
-          ),
-        ));
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
