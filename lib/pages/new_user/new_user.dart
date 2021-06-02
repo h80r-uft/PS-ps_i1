c4a1aa4b4f37ad16c4d81db95fc56afc8e901ec3
yous1964 <yous1964@hotmail.com>
2021-06-01 22:24:24 -0300

    feature/tela_cadastro | Tela de cadastro ajustada
    - Tela de cadastro ajustada
        - Tela adaptada aos padrões de Redux
        - Lógica removida da página

--- lib/pages/new_user/new_user.dart
+++ lib/pages/new_user/new_user.dart
@@ -3,6 +3,25 @@ import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 
 firebase_auth.FirebaseAuth firebaseAuth = firebase_auth.FirebaseAuth.instance;
 
+class MyInput extends StatelessWidget {
+  final String? label;
+  final String? hint;
+  final bool isPassword;
+
+  const MyInput({required this.label, this.hint, this.isPassword = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        hintText: hint,
+        labelText: label,
+      ),
+      obscureText: isPassword,
+    );
+  }
+}
+
 // Classe de widget que compõe o corpo da página de criar user
 class NewUser extends StatelessWidget {
   const NewUser({Key? key}) : super(key: key);
@@ -18,39 +37,12 @@ class NewUser extends StatelessWidget {
         child: Form(
           child: ListView(
             children: <Widget>[
-              TextFormField(
-                validator: _validaNome,
-                keyboardType: TextInputType.text,
-                decoration: const InputDecoration(
-                  labelText: "Nome",
-                  hintText: "Informe o nome",
-                ),
-              ),
-              TextFormField(
-                validator: _validaMail,
-                keyboardType: TextInputType.text,
-                decoration: const InputDecoration(
-                  labelText: "E-Mail",
-                  hintText: "Informe o e-mail",
-                ),
-              ),
-              TextFormField(
-                validator: _validaSenha,
-                obscureText: true,
-                keyboardType: TextInputType.text,
-                decoration: const InputDecoration(
-                  labelText: "Senha",
-                  hintText: "Informe a senha",
-                ),
-              ),
-              TextFormField(
-                validator: _validaSenha2,
-                obscureText: true,
-                keyboardType: TextInputType.text,
-                decoration: const InputDecoration(
-                  hintText: "Confirme a senha",
-                ),
-              ),
+              MyInput(label: 'Nome', hint: 'Informe o nome', isPassword: false),
+              MyInput(
+                  label: 'E-Mail', hint: 'Informe o e-mail', isPassword: false),
+              MyInput(
+                  label: 'Senha', hint: 'Informe a senha', isPassword: true),
+              MyInput(label: '', hint: 'Confirme a senha', isPassword: true),
               Container(
                 height: 40.0,
                 margin: const EdgeInsets.only(top: 10.0),
@@ -77,120 +69,4 @@ class NewUser extends StatelessWidget {
       ),
     );
   }
-
-  // Mensagens de erro que surgem se algum campo estiver vazio
-  String? _validaNome(String? text) {
-    if (text!.isEmpty) {
-      return "Informe o nome";
-    }
-    return null;
-  }
-
-  String? _validaMail(String? text) {
-    if (text!.isEmpty) {
-      return "Informe o endereço de e-mail";
-    }
-    return null;
-  }
-
-  String? _validaSenha(String? text) {
-    if (text!.isEmpty) {
-      return "Informe a senha";
-    }
-    return null;
-  }
-
-  String? _validaSenha2(String? text) {
-    if (text!.isEmpty) {
-      return "Confirme a senha";
-    }
-    return null;
-  }
-
-/*
-  _onClickLogin(BuildContext context) {
-    final login = _tedNome.text;
-    final mail = _tedMail.text;
-    final senha = _tedSenha.text;
-    final senha2 = _tedSenha2.text;
-
-    print("Login: $login , Mail: $mail , Senha: $senha ($senha2)  ");
-
-    if (!_formKey.currentState!.validate()) {
-      return;
-    }
-
-    if (login.isEmpty || mail.isEmpty || senha.isEmpty || senha2.isEmpty) {
-      showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: const Text("Erro"),
-            content: const Text("Todos os campos são obrigatórios"),
-            actions: <Widget>[
-              TextButton(
-                child: const Text("OK"),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              )
-            ],
-          );
-        },
-      );
-    } else {
-      signIn(
-        context,
-        login,
-        senha,
-      );
-    }
-  }
-}
-
-/// A função signIn loga um(a) usuário(a) no sistema, com base nos dados do Firestone
-void signIn(BuildContext context, login, senha) async {
-  firebaseAuth
-      .signInWithEmailAndPassword(email: login, password: senha)
-      .then((userCredential) {
-    // Signed in
-    showAlertDialog(context, "LOGADO!", login);
-    print(userCredential.user);
-    print(login + " está logade!");
-  }).catchError((error) {
-    showAlertDialog(context, "NÃO LOGADO!", "error");
-    print(error);
-    print("O login não foi concluído :(");
-  });
-}
-
-showAlertDialog(BuildContext context, titleText, contentText) {
-  Widget okButton = TextButton(
-    onPressed: () {
-      Navigator.pop(context);
-    },
-    child: const Text(
-      "OK",
-    ),
-  );
-
-  AlertDialog alerta = AlertDialog(
-    title: Text(
-      titleText,
-    ),
-    content: Text(
-      contentText,
-    ),
-    actions: [
-      okButton,
-    ],
-  );
-
-  showDialog(
-    context: context,
-    builder: (context) {
-      return alerta;
-    },
-  );
-*/
 }
