92d7c407e7e42cbb9bdda2b84a5b374fea722d08
Hernandes Carneiro Macedo <hcarneiromacedo@gmail.com>
2021-06-05 20:13:20 -0300

    feature/new_user_redux | Cadastro de novos alunos implementado
    O cadastro de novos alunos já é possível nesse ponto:
    - A função register em add_student_service_firestore.dart faz conexão com o firebase e registra alunos;

--- lib/services/add_student/add_student_service_firestore.dart
+++ lib/services/add_student/add_student_service_firestore.dart
@@ -1,53 +1,38 @@
 // Import the firebase_core and cloud_firestore plugin
-import 'dart:html';
-
+import 'package:firebase/firebase.dart' as firebase;
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import 'package:ps_i1/services/add_student/add_student_service.dart';
 
-firebase_auth.FirebaseAuth firebaseAuth = firebase_auth.FirebaseAuth.instance;
-
-/*
-final userCollection = FirebaseFirestore.instance.collection("users");
-
-User fromDocumentSnapshot(String name, String email, String password) {
-  final data = doc.data();
-
-  if (data == null || data is! Map) throw "DocumentSnapshot data error!";
-
-  final user = User(uid: doc.id, name: data['name']);
-
-  return data['isTeacher']
-      ? Teacher(user)
-      : Student(
-          user,
-          firstGrade: data['firstGrade'],
-          secondGrade: data['secondGrade'],
-        );
-}*/
-
+/// Conecta com firestore para registro de alunos
 class AddStudentServiceFirestore extends AddStudentService {
   @override
+
+  /// Registra estudante a partir de seu nome, email e senha
   Future<void> register(String name, String email, String password) async {
-    await firebaseAuth
-        .createUserWithEmailAndPassword(email: email, password: password)
+    final secondaryApp = firebase.initializeApp(
+        apiKey: "AIzaSyCzzAAm9k4eJr3TqJDl9mGx1lwHRw3zQbw",
+        authDomain: "ps-i1-9deca.firebaseapp.com",
+        projectId: "ps-i1-9deca",
+        storageBucket: "ps-i1-9deca.appspot.com",
+        messagingSenderId: "210305718179",
+        appId: "1:210305718179:web:3a1555b7ced737cd78ac16",
+        measurementId: "G-WV7DC63XJ5",
+        name: "Secondary");
+
+    /// Cria uma segunda referência para o Firebase Auth
+    await secondaryApp
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
         .then((cred) {
-      try {
-        FirebaseFirestore.instance.collection("users").doc(cred.user!.uid).set({
-          "isTeacher": false,
-          "name": name,
-        });
-      } catch (error2) {
-        print("Erro ao add doc user: ");
-        print(error2);
-      }
+      FirebaseFirestore.instance
+          .collection("users")
+          .doc(cred.user!.uid)
+          .set({"isTeacher": false, "name": name});
+      secondaryApp.auth().signOut();
+      secondaryApp.delete();
     }).onError((error, stackTrace) {
-      print("Erro ao criar user");
       print(error);
     });
-    print("-------------------------");
-    print("EMAIL DO USER ATUAL:");
-    print(firebaseAuth.currentUser!.email);
     return null;
   }
 }
